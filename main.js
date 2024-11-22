0605cb69d0879f5cb634eac9ce134c90fd155160
Orashi6 <bekb0323@gmail.com>
2024-11-22 12:59:49 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+// let names = prompt('ismingizni kiriting')
+// let firstNumber = prompt('birinchi raqamni kirgizing ')
+// let secondNumber = prompt('ikkinchi raqamni kirgizing')
+
+// let sliced = names.slice(firstNumber , secondNumber )
+
+
+// let javob =  alert(sliced.toUpperCase(sliced))
+
+// console.log(javob);
+
+
+
+
+
+
+let soz = prompt('bironta soz kiriting ')
+
+
+
+ soz.trim()
+
+ let uzunligi = soz.length
+
+if ( uzunligi >= 10  ) {
+
+ let alertJavobi =   alert(`kop yozding`)
+} else if ( uzunligi == 0 ) {
+  let alertJavobi2 =  alert(`sozni yoz`)
+}
+
+
+
+console.log(alertJavobi);
+
+
+console.log(alertJavobi2);
+
